3bee5aed590cc32e88a0f4a6820a3a993f976ec1
mmitropolitsky <milkomitropolitsky@gmail.com>
2019-10-26 14:49:47 +0200

    Slight refactor

--- src/main/scala/models/FlatFlights.scala
+++ src/main/scala/models/FlatFlights.scala
@@ -2,8 +2,15 @@ package models
 
 import java.time.LocalDateTime
 
-case class FlatFlight(departure: String, arrival: String, carrier: String,
-                       departureExpectedTimeUTC: LocalDateTime, departureActualTimeUTC: LocalDateTime,
-                       departureExpectedTimeLocal: LocalDateTime, departureActualTimeLocal: LocalDateTime,
-                       arrivalExpectedTimeUTC: LocalDateTime, arrivalActualTimeUTC: LocalDateTime,
-                       arrivalExpectedTimeLocal: LocalDateTime, arrivalActualTimeLocal: LocalDateTime)
+case class FlatFlight(airportDateKey: String,
+                      departure: String,
+                      arrival: String,
+                      carrier: String,
+                      departureExpectedTimeUTC: LocalDateTime,
+                      departureActualTimeUTC: LocalDateTime,
+                      departureExpectedTimeLocal: LocalDateTime,
+                      departureActualTimeLocal: LocalDateTime,
+                      arrivalExpectedTimeUTC: LocalDateTime,
+                      arrivalActualTimeUTC: LocalDateTime,
+                      arrivalExpectedTimeLocal: LocalDateTime,
+                      arrivalActualTimeLocal: LocalDateTime)
