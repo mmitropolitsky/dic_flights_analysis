df0314a53b8c83e0dceb5aad2b1268be2d881211
viktoriya-kutsarova <viktoriya.kutsarova@gmail.com>
2019-10-27 22:25:30 +0100

    Push new airport coordinates.

--- src/main/scala/models/Airport.scala
+++ src/main/scala/models/Airport.scala
@@ -4,9 +4,9 @@ object Airport {
 
   // It is enough to put {latitude},{longitude} pair. It will be added to the URL
   val airportCodeToCoordinatesMap: Map[String, String] = Map(
+    "LHR" -> "51.4700256,-0.4564895",
     "SOF" -> "42.6934317,23.4049293",
-    "ARN" -> "59.3262416,17.8416281",
-    "FRA" -> "50.1213475,8.4961381"
+    "ARN" -> "59.3262416,17.8416281"
   )
 
 }
